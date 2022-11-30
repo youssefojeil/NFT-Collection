a03daaf05d1bc2218c28468250475514dce6b00c
unknown <youssef.ojeil@hotmail.com>
2022-11-29 22:58:23 -0500

    create wallet connect function and button, presale started, pre sale ended functions

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x9385A0324098043184427526CF8fC935004967CF";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
